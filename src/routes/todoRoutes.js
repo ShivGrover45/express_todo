60c66798d1c3daf9a24640b8afeccb9849567576
ShivGrover45 <codershiv16@gmail.com>
2024-12-24 23:51:13 +0530

    changes added

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -12,13 +12,34 @@ todoRoutes.get('/',(req,res)=>{
 })
 
 //create todos for users
-todoRoutes.post('/',(req,res)=>{})
+todoRoutes.post('/',(req,res)=>{
+    const {tasks}=req.body
+    const insertTodo=db.prepare(`INSERT INTO todos (user_id,tasks) VALUES (?,?)`)
+   const result= insertTodo.run(req.userId,tasks)
+    res.json({id: result.lastInsertRowid, tasks, completed:0})
+})
 
 //for updating tasks
-todoRoutes.put('/:id',(req,res)=>{})
+todoRoutes.put('/:id',(req,res)=>{
+    const {completed}=req.body
+    const {id}=req.params
+    const {page}=req.query
+    const updateTodo=db.prepare(`
+        UPDATE todos SET completed=? WHERE id=?
+        `)
+        updateTodo.run(completed,id)
+        res.json({message:"Task completed"})
+})
 
 //for deleting tasks
 todoRoutes.delete('/:id',(req,res)=>{
+    const {id}=req.params
+    const userId=req.userId
+    const deleteTodo=db.prepare(`
+        DELETE FROM todos WHERE id=? AND user_id=?
+        `)
+        deleteTodo.run(id,userId)
+        res.json({message:"Task deleted"})
 })
 
 export default todoRoutes
