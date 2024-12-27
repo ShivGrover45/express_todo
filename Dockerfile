4bafc3e99f5c34dcdcfe09f3eec993dac9aaa90d
ShivGrover45 <codershiv16@gmail.com>
2024-12-27 17:32:22 +0530

    bug fixed

--- Dockerfile
+++ Dockerfile
@@ -17,4 +17,4 @@ COPY . .
 EXPOSE 8000
 
 #running the app
-CMD ["node",",/src/server.js"]
+CMD ["node","./src/server.js"]
--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -2,10 +2,11 @@ import jwt from 'jsonwebtoken'
 
 function authMiddleWare(req,res,next){
     const token=req.headers['authorization']
-    if(!token){res.status(401).json({message:"Token not available"})}
+    if(!token){
+        return res.sendStatus(401).json({message:"Token not available"})}
     jwt.verify(token,process.env.JWT_SECRET,(err,decoded)=>{
         if(err){
-            return res.send(401).json({message:"Invalid Token"})
+            return res.sendStatus(401).json({message:"Invalid Token"})
         }
         req.userId=decoded.id
         next()
